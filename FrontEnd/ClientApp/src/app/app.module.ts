bb5cbe41561263d77c3c1a3f2d1e67a4800cda37
Алексей Данилин <saribander@ya.ru>
2019-10-10 22:32:31 +0500

    cleaning up all solution's code

--- FrontEnd/ClientApp/src/app/app.module.ts
+++ FrontEnd/ClientApp/src/app/app.module.ts
@@ -6,11 +6,6 @@ import { RouterModule } from '@angular/router';
 import { DatePipe } from '@angular/common';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { OrdersPageComponent } from './orders-page/orders-page.component';
 import { OrderWrapperComponent } from './order-wrapper/order-wrapper.component';
 import { OrderOverviewComponent } from './order-wrapper/order-overview/order-overview.component';
 import { OrderSummaryComponent } from './order-wrapper/order-summary/order-summary.component';
@@ -19,11 +14,6 @@ import { OrderDetailsComponent } from './order-wrapper/order-details/order-detai
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,    
-    OrdersPageComponent,
         OrderWrapperComponent,
         OrderOverviewComponent,
         OrderSummaryComponent,
@@ -33,11 +23,10 @@ import { OrderDetailsComponent } from './order-wrapper/order-details/order-detai
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'orders-page', component: OrdersPageComponent },
+      RouterModule.forRoot([
+          { path: '', redirectTo: '/orders', pathMatch: 'full' },
+          { path: '**', redirectTo: '/orders' },
+          { path: 'orders', component: OrderWrapperComponent },
     ])
   ],
   providers: [DatePipe],
--- FrontEnd/ClientApp/src/app/orders-page/orders-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'orders-page',
-  templateUrl: './orders-page.component.html',
-  styleUrls: ['./orders-page.component.css']
-})
-export class OrdersPageComponent {
-}
